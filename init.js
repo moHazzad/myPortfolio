f481b057b4434600f1347fb0ee5b30a1e8dd2ea9
Mo Hazzad <hazzadmdap@gmail.com>
2022-11-30 14:29:54 +0300

    all html css aded home

---
+++ init.js
@@ -0,0 +1,18 @@
+// // arlo_tm_animate_text()
+// arlo_tm_animate_text()
+
+
+
+// function arlo_tm_animate_text(){
+	
+// 	"use strict";
+	
+// 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
+	
+// 		animateSpan.typed({
+// 			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+// 			loop: true,
+// 			startDelay: 1e2,
+// 			backDelay: 2e2
+// 		});
+// }
---
+++ script.js
@@ -0,0 +1,25 @@
+// const text = document.querySelector(`.sec-text`);
+
+// const TextLoad =() =>{
+//   setTimeout(() => {
+    
+//     text.textContent = `Freelancer`;
+
+
+//   }, 0);
+//   setTimeout(() => {
+    
+//     text.textContent = `Web developer`;
+
+
+//   }, 4000);
+//   setTimeout(() => {
+    
+//     text.textContent = `Web Designer`;
+
+
+//   }, 8000);
+  
+// }
+// TextLoad();
+// setInterval(TextLoad, 12000);
